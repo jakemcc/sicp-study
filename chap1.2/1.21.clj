b494490e8b768a8c68afa4f18cd937893f618971
Jake McCrary <jakemcc@gmail.com>
2009-10-10 16:12:34 -0500

    Changed 1.21 to use recur

--- chap1.2/1.21.clj
+++ chap1.2/1.21.clj
@@ -9,7 +9,7 @@
 (defn find-divisor [n test-divisor]
  (cond (> (square test-divisor) n) n
        (divides? test-divisor n) test-divisor
-       :else (find-divisor n (+ test-divisor 1))))
+       :else (recur n (+ test-divisor 1))))
 
 (defn smallest-divisor [n]
  (find-divisor n 2))
