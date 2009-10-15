f30d9809bafe585937a6123b4ebe46a5eb47650e
Jake McCrary <jakemcc@gmail.com>
2009-10-14 22:49:52 -0500

    Finish 1.25

--- chap1.2/1.25.clj
+++ chap1.2/1.25.clj
@@ -1,1 +1,10 @@
 ; Exercise 1.25
+; 
+; expmod prevents us from doing the computation with very
+; large numbers.  Extremely large numbers could cause
+; issues for the process as they may have to be dealt with
+; specially.
+;
+; Large numbers would be an issue with fast-exp
+;
+; Footnote 46 pretty much says this but with more words.
