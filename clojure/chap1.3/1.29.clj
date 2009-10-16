b47218f2500c80fb7da77b437c31bf27d1b12a93
Jake McCrary <jakemcc@gmail.com>
2009-10-16 16:39:31 -0500

    more work

--- clojure/chap1.3/1.29.clj
+++ clojure/chap1.3/1.29.clj
@@ -15,3 +15,18 @@
 
 (integral cube 0 1 0.01)
 (integral cube 0 1 0.001)
+
+
+
+(defn h [a b n]
+ (/ (- b a) n))
+
+; (defn simpsons-rule [f a b n]
+;  (let [h (/ (- b a) n)
+;        helper (fn [k accum]
+;            (let [x ((+ a (* k h)))]
+;             (cond (= k 0) (helper (inc k) (+ accum (f x)))
+;                   (= k n) (+ accum (f x))
+;                   (even? k) (+ (* 2 (f x)) (helper (inc k) accum))
+;                   :else (+ (* 4 (f x)) (helper (inc k) accum)))))]
+;        (helper 0 0)))
