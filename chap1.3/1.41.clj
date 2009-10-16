ca5a9180aff4f5d41f06caecc0d966ef6c10c332
Jake McCrary <jakemcc@gmail.com>
2009-10-15 22:55:36 -0500

    Finished problems 1.41 through 1.43.

---
+++ chap1.3/1.41.clj
@@ -0,0 +1,15 @@
+; Exercise 1.41
+
+; Define a procedure called double (my-double) which takes a procedure
+; as one argument and retuns a procedure that applies the 
+; original twice
+
+(defn my-double [f]
+ (fn [x] (f (f x))))
+
+; What value is returned by
+; (((my-double (my-double my-double)) inc) 5)
+; 21 
+
+(((my-double (my-double my-double)) inc) 5)
+; 21
