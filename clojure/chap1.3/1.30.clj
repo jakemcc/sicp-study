0232c62b0aea3eb5581726064d11782fadcb6069
Jake McCrary <jakemcc@gmail.com>
2009-10-18 23:05:56 -0500

    Finished exercise 1.30

--- clojure/chap1.3/1.30.clj
+++ clojure/chap1.3/1.30.clj
@@ -1,16 +1,25 @@
 ; Exercise 1.30
 ;
+
+(defn cube [x] (* x x x))
 ; Linear recusive version
-; (define (sum term a next b)
-;   (if (> a b)
-;        0
-;       (+ (term a)
-;          (sum term (next a) next b))))
+(defn sum [term a nxt b]
+ (if (> a b)
+      0
+     (+ (term a)
+        (sum term (nxt a) nxt b))))
 
+(defn sum-cubes [a b]
+ (sum cube a inc b))
 
+(sum-cubes 1 10)
+
+; Iterative version
 (defn sum [term a nxt b]
  (let [iter (fn [a result]
-              (if <??>
-                  <??>
-                  (recur <??> <??>)))]
-  (iter <??> <??>)))
+              (if (> a b)
+                  result
+                  (recur (nxt a) (+ result (term a)))))]
+  (iter a 0)))
+
+(sum-cubes 1 10)
