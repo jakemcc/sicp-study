1ae3d43a2495511c47f7a8c76f207738a612dad3
Jake McCrary <jakemcc@gmail.com>
2009-10-24 10:35:25 -0500

    Did exercise 2.6

---
+++ clojure/chap2.1/2.6.clj
@@ -0,0 +1,33 @@
+; Exercise 2.6
+
+(def zero (fn [f] (fn [x] x)))
+
+(defn add-1 [n]
+ (fn [f] (fn [x] (f ((n f) x)))))
+
+; (add-1 zero)
+; (add-1 (fn [f] (fn [x] x)))
+; (fn [f] (fn [x] (f (((fn [f] (fn [x] x))) f) x))))
+; (fn [f] (fn [x] (f x))    == one
+
+(def one (fn [f] (fn [x] (f x))))
+
+; (add-1 one)
+; (add-1 (fn [f] (fn [x] (f x))))
+; (fn [f] (fn [x] (f ((fn [f] (fn [x] (f x))) f) x)))
+; (fn [f] (fn [x] (f ((fn [x] (f x))) x)))
+; (fn [f] (fn [x] (f (f x))))    == two
+
+(def two (fn [f] (fn [x] (f (f x)))))
+
+(defn- compose [f g]
+ (fn [x] (f (g x))))
+
+; Modeled after add-1.  add-1 passes f
+; as an argument to number passed to it.
+; Compose the two numbers to get them to
+; be more nexted f calls
+(defn add-church [a b]
+ (fn [f]
+  (compose (a f) (b f))))
+
