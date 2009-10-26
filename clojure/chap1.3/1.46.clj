3f4c94f5f0cb48ecb78a9b46b6b8699524ea44b7
Jake McCrary <jakemcc@gmail.com>
2009-10-26 12:48:36 -0500

    Finished up exercise 1.46

--- clojure/chap1.3/1.46.clj
+++ clojure/chap1.3/1.46.clj
@@ -21,3 +21,17 @@
 
 (println (sqrt 4))
 (println (sqrt 16))
+
+; Redefine fixed-point of section 1.3.3 to use iterative-improve
+(defn fixed-point [f first-guess]
+ ((iterative-improve
+   (fn [g1 g2] (< (Math/abs (- g1 g2)) 0.00001))
+   f)
+  first-guess))
+
+; Procedure to show that fixed-point works
+(defn calculate-golden-ratio
+ []
+ (fixed-point (fn [x] (+ 1 (/ 1 x)))
+              1.0))
+(println (calculate-golden-ratio))
