2b08e36db864c01501578d3a2aeb383d62f87b2a
Jake McCrary <jakemcc@gmail.com>
2009-11-01 17:00:37 -0600

    Did as much of problem 2.24 as I care to do

---
+++ clojure/section2.2/2_24.clj
@@ -0,0 +1,14 @@
+; Exercise 2.24
+
+; Clojure=> (list 1 (list 2 (list 3 4)))
+; (1 (2 (3 4)))
+;
+;
+; Tree below.   Not going to show boxes
+;          (1 (2 (3 4)))
+;           /         \
+;          1       (2 (3 4))
+;                   /  \
+;                  2  (3 4)
+;                      / \
+;                      3  4
