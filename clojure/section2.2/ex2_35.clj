f7b5247923537b5fb1a8518fc8547c51f2776630
Jake McCrary <jmccrary@wud-jmccrary01.us.drwholdings.com>
2009-11-23 18:23:11 -0600

    finished exercise 35 and did work on queen

--- clojure/section2.2/ex2_35.clj
+++ clojure/section2.2/ex2_35.clj
@@ -1,17 +1,21 @@
 ; Exercise 2.35
-; not working
 (use 'clojure.test)
 
-;; (defn accumulate [op initial sq]
-;;  (if (empty? sq)
-;;      initial
-;;      (op (first sq)
-;;          (accumulate op initial (rest sq)))))
-;; 
-;; (defn count-leaves [t]
-;;  (accumulate (fn [x y] (inc y)) 0 (map count t)))
-;; 
-;; (deftest test-count-leaves
-;;  (is (= 3 (count-leaves '(1 (2 3))))))
-;; 
-;; (run-tests)
+(defn accumulate [op initial sq]
+ (if (empty? sq)
+     initial
+     (op (first sq)
+         (accumulate op initial (rest sq)))))
+
+(defn count-leaves [t]
+ (accumulate + 0 (map (fn [x]
+                       (if (seq? x) 
+                             (count-leaves x)
+                            1))
+                        t)))
+
+(deftest test-count-leaves
+ (is (= 3 (count-leaves '(1 (2 3)))))
+ (is (= 5 (count-leaves '((8 7 9) (2 3))))))
+
+(run-tests)
--- clojure/section2.2/ex2_42.clj
+++ clojure/section2.2/ex2_42.clj
@@ -1,5 +1,6 @@
-(defn accumulate [op initial sq]
- (if (empty? sq)
+(use 'clojure.test)
+
+(defn accumulate [op initial sq] (if (empty? sq)
      initial
      (op (first sq)
          (accumulate op initial (rest sq)))))
@@ -14,10 +15,64 @@
       res
 	  (recur (dec cur) (cons cur res)))))
 
+(def empty-board nil)
 
+(defn adjoin-positions [new-row k rest-of-queens] 
+ (map (fn [c] (cons (list new-row k) c))
+      rest-of-queens))
+
+(defn all? [t]
+ (reduce (fn [x y] (and x y))
+         true
+         t))
+
+(defn safe-line? [op potential]
+ (let [f (first potential)
+       r (rest potential)]
+  (all? (map (fn [x] (not (= (op f) (op x)))) r))))
+
+(defn safe-x? [potential]
+ (safe-line? first potential))
+
+(deftest safe-x?-works
+ (is (= true (safe-x? '((1 2) (0 0)))))
+ (is (= false (safe-x? '((1 2) (1 3))))))
+
+
+(defn safe-y? [potential]
+ (safe-line? second potential))
+
+(deftest safe-y?-works
+ (is (= true (safe-y? '((1 2) (0 0)))))
+ (is (= false (safe-y? '((1 1) (3 1))))))
+
+(defn safe-diag? [potential]
+ (let [f (first potential)
+       r (rest potential)]
+   (all? (map (fn [x] (not (= 1 
+                              (Math/abs (double 
+                                         (/
+                                           (- (first f) (first x))
+                                           (- (second f) (second x))))))))
+          r))))
+
+(deftest safe-diag?-works
+ (is (= true (safe-diag? '((1 2) (0 0)))))
+ (is (= false (safe-diag? '((1 1) (0 0)))))
+ (is (= false (safe-diag? '((0 0) (1 1))))))
+ 
+
+(defn safe? [k positions]
+ (and (safe-x? positions) (safe-y? positions) (safe-diag? positions)))
+
+
+(deftest safe?-works
+ (is (= false (safe? 2 '((1 0) (0 0))))) 
+ (is (= false (safe? 2 '((1 1) (0 0))))) 
+ (is (= true (safe? 2 '((1 2) (0 0))))) )
 
 (defn queens [board-size]
- (let [queen-cols (fn [k]
+ (let [queen-cols (fn queen-cols [k]
                    (if (= k 0)
                        (list empty-board)
                        (filter
@@ -25,13 +80,12 @@
                         (flatmap
                          (fn [rest-of-queens]
                            (map (fn [new-row]
-                                  (adjoin-poisition new-row k rest-of-queens))
+                                  (adjoin-positions new-row k rest-of-queens))
                                 (enumerate-interval 1 board-size)))
-                           (recur (- k 1))))))]
+                           (queen-cols (- k 1))))))]
   (queen-cols board-size)))
 
-(defn adjoin-positions [new-row k rest-of-queens] )
+(run-tests)
 
-(def empty-board nil)
-
-(defn safe? [k positions] )
+(println (count (queens 4)))
+(println (queens 4))
