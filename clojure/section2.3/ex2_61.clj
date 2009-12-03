55ecfaba7969a743bfedca6c3f1e6f74700b0ef0
Jake McCrary <jakemcc@gmail.com>
2009-12-02 21:07:33 -0600

    finished exercise 2.61

---
+++ clojure/section2.3/ex2_61.clj
@@ -0,0 +1,41 @@
+; Exercise 2.61
+(use 'clojure.test)
+
+; Now use ordered lists to represent sets
+
+
+
+; Not using any nice things in Clojure
+(defn adjoin-set [x s]
+ (loop [head '()
+        tail s]
+  (cond (empty? tail) (concat head (list x))
+        (= x (first tail)) (concat head tail)
+        (> x (first tail)) (recur (concat head 
+                                          (list (first tail)))
+                                  (rest tail))
+        :else (concat head (list x) tail))))
+
+(deftest adjoin-set-puts-low-element-at-beginning
+ (is (= '(1 3 4) (adjoin-set 1 '(3 4)))))
+
+(deftest adjoin-set-puts-element-in-middle
+ (is (= '(2 3 4) (adjoin-set 3 '(2 4)))))
+
+(deftest adjoin-set-puts-large-element-at-end
+ (is (= '(1 3 4 5) (adjoin-set 5 '(1 3 4)))))
+
+(run-tests)
+  
+(defn element-of-set? [x s]
+ (cond (empty? s) false
+       (= x (rest s)) true
+       (< x (first s)) false
+       :else (element-of-set? x (rest s))))
+
+; Somewhat cheating
+(defn adjoin-set [x s]
+ (if (element-of-set? x s) s
+     (sort (cons x s))))
+
+(run-tests)
