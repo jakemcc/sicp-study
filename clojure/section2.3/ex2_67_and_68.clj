bcbe99fc153b2be1b1011299dd0b590961857185
xbmc <xbmc@jtv.(none)>
2009-12-08 21:38:52 -0600

    finished exercise 2.68

--- clojure/section2.3/ex2_67_and_68.clj
+++ clojure/section2.3/ex2_67_and_68.clj
@@ -1,4 +1,6 @@
-; Exercise 2.67
+; Exercise 2.67 and 2.68
+
+(use 'clojure.test)
 
 ;{{{ Needed supporting functions
 (defn make-leaf [sym weight]
@@ -43,6 +45,9 @@
   (decode-1 bits tree)))
 ;}}}
 
+
+; Problem 2.67
+
 (def sample-tree
  (make-code-tree (make-leaf 'A 4)
                  (make-code-tree
@@ -52,10 +57,44 @@
 
 (def sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
-
 ; Question: What is the message in sample-message
 ;
 ; Answer:
-(println (decode sample-message sample-tree))
+(deftest decode-works-on-samples
+ (is (= '(A D A B B C A) (decode sample-message sample-tree))))
 
+
+; Exercise 2.68
+; Finish an encode function.  Test against solution from 2.67
+    
+(defn encode-symbol [elem tree]
+ (letfn [(contains-e? [syms] (some #(= % elem) syms))]
+  (cond (and (leaf? tree)) '()
+        (contains-e? (symbols (right-branch tree))) 
+          (cons 1 (encode-symbol elem (right-branch tree)))
+        (contains-e? (symbols (left-branch tree))) 
+          (cons 0 (encode-symbol elem (left-branch tree)))
+        :else 'error)))
+
+
+(deftest encode-symbol-finds-element
+ (is (= '(0) (encode-symbol 'A sample-tree)))
+ (is (= '(1 1 0) (encode-symbol 'D sample-tree)))
+ (is (= '(1 1 1) (encode-symbol 'C sample-tree)))
+ (is (= '(1 0) (encode-symbol 'B sample-tree))))
+
+(deftest encode-puts-error-if-no-encoding-found
+ (is (= 'error (encode-symbol 'X sample-tree))))
+
+(defn encode [message tree]
+ (if (empty? message)
+     '()
+     (concat (encode-symbol (first message) tree)
+             (encode (rest message) tree))))
+
+(deftest encode-works-on-samples
+ (is (= sample-message (encode (decode sample-message sample-tree) sample-tree))))
+
+
+(run-tests)
 ; vim: set foldmethod=marker:
