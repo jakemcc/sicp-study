8ac177306347992867861e157764334637abb0f9
Jake McCrary <jakemcc@gmail.com>
2009-12-21 16:56:46 -0600

    finished exercise 2.79

---
+++ clojure/section2.5/ex2_78.clj
@@ -0,0 +1,21 @@
+; Exercise 2.79
+; The below code wont run.  Have not acctually defined all of the
+; procedures used by the below code.  This is more of a thought
+; exercise with some example code.
+
+(defn equ? [x y]
+ (apply-generic 'equ? x y))
+
+; Put this with the scheme-number installers
+(put 'equ? '(scheme-number scheme-number)
+ (fn [x y] (= x y)))
+
+; Put this with the rational package installers
+(put 'equ? '(rational rational)
+ (fn [x y] (and (= (numer x) (numer y))
+                (= (denom x) (denom y)))))
+
+; Put in complex installers
+(put 'equ? '(complex complex)
+ (fn [x y] (and (= (real-part x) (real-part y))
+                (= (imag-part x) (imag-part y)))))
