a016e5ce4ab58dd7a4f332fb387c5d22143a87d8
Jake McCrary <jakemcc@gmail.com>
2009-12-31 16:05:53 -0600

    reread exercise 3.2 and realized I forgot to do part of it, it is actually finished now :)

--- clojure/section3.1/ex3_2.clj
+++ clojure/section3.1/ex3_2.clj
@@ -5,16 +5,18 @@
 (defn make-monitored [f]
  (let [num-calls (atom 0)]
   (fn [x]
-   (if (= x 'how-many-calls?)
-       @num-calls
-       (do 
-        (swap! num-calls inc)
-        (f x))))))
+   (cond (= x 'how-many-calls?) @num-calls
+         (= x 'reset-calls) (swap! num-calls (fn [x] 0))
+         :else (do
+                (swap! num-calls inc)
+                (f x))))))
 
 (deftest test-make-monitored
  (let [m-inc (make-monitored inc)]
   (is (= 1 (m-inc 0)))
   (is (= 5 (m-inc 4)))
-  (is (= 2 (m-inc 'how-many-calls?)))))
+  (is (= 2 (m-inc 'how-many-calls?)))
+  (m-inc 'reset-calls)
+  (is (= 0 (m-inc 'how-many-calls?)))))
 
 (run-tests)
