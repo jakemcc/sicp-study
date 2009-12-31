4442f8ff3f95340c924630caf76bdf9537d94285
Jake McCrary <jakemcc@gmail.com>
2009-12-31 15:59:55 -0600

    cleaned up some return values

--- clojure/section3.1/ex3_3.clj
+++ clojure/section3.1/ex3_3.clj
@@ -8,7 +8,7 @@
             (if (>= @balance amount)
                 (do (swap! balance #(- % amount))
                     @balance)
-                "insufficient-funds"))
+                "insufficient funds"))
            (deposit [amount]
              (swap! balance #(+ % amount))
              @balance)
@@ -17,18 +17,18 @@
                  (cond (= action 'withdraw) withdraw
                        (= action 'deposit) deposit
                        :else (Error. (str "Unknown request -- MAKE-ACCOUNT " action)))
-                 (fn [x] "incorrect-password")))]
+                 (fn [x] "incorrect password")))]
     dispatch)))
 
 
 (deftest test-with-correct-password
  (let [acc (make-account 100 'secret-password)]
   (is (= 60 ((acc 'secret-password 'withdraw) 40)))
-  (is (= "insufficient-funds" ((acc 'secret-password 'withdraw) 400)))
+  (is (= "insufficient funds" ((acc 'secret-password 'withdraw) 400)))
   (is (= 100 ((acc 'secret-password 'deposit) 40)))))
 
 (deftest test-with-incorrect-password
  (let [acc (make-account 100 'secret-password)]
-  (is (= "incorrect-password" ((acc 'wrong-pw 'deposit) 50)))))
+  (is (= "incorrect password" ((acc 'wrong-pw 'deposit) 50)))))
 
 (run-tests)
