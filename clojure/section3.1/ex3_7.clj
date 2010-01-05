994c71af59c31e2e7b3239fc51695d677dac0f7b
Jake McCrary <jakemcc@gmail.com>
2010-01-04 21:52:55 -0600

    Finished exercise 3.7.

--- clojure/section3.1/ex3_7.clj
+++ clojure/section3.1/ex3_7.clj
@@ -1,7 +1,9 @@
-; Exercise 3.3
+; Exercise 3.7
+; Add the ability to have joint bank accounts to exercise 3.3.
 
 (use 'clojure.test)
 
+;{{{ make-account from exercise 3.3
 (defn make-account [starting-balance password]
  (let [balance (atom starting-balance)]
   (letfn [(withdraw [amount]
@@ -19,15 +21,22 @@
                        :else (Error. (str "Unknown request -- MAKE-ACCOUNT " action)))
                  (fn [x] "incorrect password")))]
     dispatch)))
+;}}}
 
-(deftest test-with-correct-password
- (let [acc (make-account 100 'secret-password)]
-  (is (= 60 ((acc 'secret-password 'withdraw) 40)))
-  (is (= "insufficient funds" ((acc 'secret-password 'withdraw) 400)))
-  (is (= 100 ((acc 'secret-password 'deposit) 40)))))
 
-(deftest test-with-incorrect-password
- (let [acc (make-account 100 'secret-password)]
-  (is (= "incorrect password" ((acc 'wrong-pw 'deposit) 50)))))
+(defn make-joint
+ "Makes a bank account jointly owned"
+ [account owners-pw joint-pw]
+ (fn [password action]
+  (if (= joint-pw password) (account owners-pw action)
+      (Error. (str "BAD JOINT PASSWORD -- " password)))))
+
+(deftest can-make-joint-account
+ (let [orig-acc (make-account 100 :password)
+       joint-acc (make-joint orig-acc :password :joint)]
+  (is (= 91 ((orig-acc :password 'withdraw) 9)))
+  (is (= 80 ((joint-acc :joint 'withdraw) 11)))))
 
 (run-tests)
+
+; vim:set foldmethod=marker
