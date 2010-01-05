b395071d8a7460dd71cc2f49147070df3a3f16e8
Jake McCrary <jakemcc@gmail.com>
2010-01-04 21:17:42 -0600

    Finished up exercise 3.6. Made it fit constructs specified in book instead of doing my own thing.

--- clojure/section3.1/ex3_6.clj
+++ clojure/section3.1/ex3_6.clj
@@ -5,29 +5,31 @@
 
 (use 'clojure.test)
 
-(defn rand-update [x]
+(defn- rand-update [x]
  (let [a 5.2 b 3.1 m 1.5]
   (mod (+ (* a x) b) m)))
 
-(defn get-random-sequence []
- (let [initial-val 5.0
-       current (atom initial-val)]
-  (fn [action]
-   (if (= action :reset) (swap! current (fn [x] initial-val))
-       (do
-         (swap! current rand-update)
-         @current)))))
+(def current-random (atom 5.0))
+
+(defn my-rand
+ ([] 
+  (swap! current-random rand-update)
+  @current-random)
+ ([action]
+  (if (= action :generate) (my-rand)))
+ ([action seed]
+  (if (= action :reset) (swap! current-random (fn [x] seed)))))
 
 
 (deftest test-reset-random-sequence
- (let [random-seq (get-random-sequence)
-       orig (list (random-seq :generate)
-                  (random-seq :generate)
-                  (random-seq :generate))]
-  (random-seq :reset)
-  (let [after-reset (list (random-seq :generate)
-                          (random-seq :generate)
-                          (random-seq :generate))]
+ (my-rand :reset 6.0)
+ (let [orig (list (my-rand :generate)
+                  (my-rand :generate)
+                  (my-rand :generate))]
+  (my-rand :reset 6.0)
+  (let [after-reset (list (my-rand :generate)
+                          (my-rand :generate)
+                          (my-rand :generate))]
    (is (= orig after-reset)))))
 
 (run-tests)
