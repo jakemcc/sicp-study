b9fa17a4a58ed4c97ed1b8a022a6515b14de2a01
Jake McCrary <jakemcc@gmail.com>
2010-01-24 21:41:58 -0600

    cleaned up some messy code

--- clojure/section2.3/ex2_65.clj
+++ clojure/section2.3/ex2_65.clj
@@ -19,18 +19,18 @@
 (defn partial-tree [elts n]
   (if (= n 0)
       (cons '() elts)
-      (let [left-size (quotient (- n 1) 2)]
-        (let [left-result (partial-tree elts left-size)]
-          (let [left-tree (first left-result)
-                non-left-elts (rest left-result)
-                right-size (- n (+ left-size 1))]
-            (let [this-entry (first non-left-elts)
-                  right-result (partial-tree (rest non-left-elts)
-                                              right-size)]
-              (let [right-tree (first right-result)
-                    remaining-elts (rest right-result)]
-                (cons (make-tree this-entry left-tree right-tree)
-                      remaining-elts))))))))
+      (let [left-size       (quotient (- n 1) 2)
+            left-result     (partial-tree elts left-size)
+            left-tree       (first left-result)
+            non-left-elts   (rest left-result)
+            right-size      (- n (+ left-size 1))
+            this-entry      (first non-left-elts)
+            right-result    (partial-tree (rest non-left-elts)
+                                          right-size)
+            right-tree      (first right-result)
+            remaining-elts  (rest right-result)]
+         (cons (make-tree this-entry left-tree right-tree)
+               remaining-elts))))
 
 (defn list->tree [elements]
   (first (partial-tree elements (count elements))))
