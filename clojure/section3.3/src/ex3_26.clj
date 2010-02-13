0eb9361cfdcc622d60d9fb311ffa2b32132e86fd
Jake McCrary <jakemcc@gmail.com>
2010-02-13 10:15:01 -0600

    finished 3.26

---
+++ clojure/section3.3/src/ex3_26.clj
@@ -0,0 +1,12 @@
+; Exercise 3.26
+;
+; Describe how you could define tables using a binary tree
+;
+;
+; Answer:
+;  One way to do this would be to take the keys
+;  and combine them into a single key. Then use
+;  the single key to decide where to place the key
+;  and value in the binary tree.  Each node of the
+;  binary tree would be a pair of combined keys and
+;  value.
