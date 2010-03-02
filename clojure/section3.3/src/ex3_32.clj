69ebb1db285b74204cb4b5ac275146077a63fa19
Jake McCrary <jakemcc@gmail.com>
2010-03-01 21:11:40 -0600

    sort of finished exercise 3.32. If anyone has better insight let me know

---
+++ clojure/section3.3/src/ex3_32.clj
@@ -0,0 +1,11 @@
+; Exercise 3.32
+
+; I'm having a hard time seeing how it matters.  I can see from their example
+; in the book that the output signal would switch to 1 when it really should not.
+
+; Unfortunately I'm having a difficult time seeing how if instead of going from
+; 0,1 to 1,0 if the inputs changed from 1,0 to 0,1 how the same problem wouldn't
+; happen with the first in, first out behavior of using queues.
+
+; Unless there is a way to make every action happen at the same time, I have a hard
+; time seeing how this type of race condition is fixable. (that may be whiskey talking)
