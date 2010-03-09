5e7418adf779824d28f8a22ff9ed5df01ff65c3d
Jake McCrary <jakemcc@gmail.com>
2010-03-08 21:32:01 -0600

    finished exercise 3.41

---
+++ clojure/section3.4/ex3_41.clj
@@ -0,0 +1,9 @@
+; Exercise 3.41
+
+; I disagree with Ben Bitdiddle.
+; I believe that allowing any one to look at the balance at anytime through
+; the dispatch function while a deposit or withdraw is going on is alright.
+; The potential to screw up the actual value in the account does not exist
+; as viewing the balance is only viewing. Sure you may view in the middle
+; of a transaction but that is not wrong, in that instant the account has
+; that amount of money.
