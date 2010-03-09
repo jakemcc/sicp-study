79a94b280df7d417a05f937b6b1ebdd15a632596
Jake McCrary <jakemcc@gmail.com>
2010-03-08 21:47:55 -0600

    finished exercises 3.44 and 3.45

---
+++ clojure/section3.4/ex3_44.clj
@@ -0,0 +1,10 @@
+; Exercise 3.44
+
+; Ben's suggestion will work.
+; The difference is that in the exchange problem there is copmutation of
+; the difference between the accounts. This allows there to be inconsistant
+; states of the balance of the accounts.
+
+; With transfer, the amount to transfer is already known. As long as all calls
+; to transfer are given time to finish then whatever amount is owed (or owed by)
+; an account will eventually get deposited (or withdrawn).
---
+++ clojure/section3.4/ex3_45.clj
@@ -0,0 +1,4 @@
+; Exercise 3.45
+
+; By exposing the serializer and seralizing internally deadlock will occur
+; if serialized-exchange is called.
