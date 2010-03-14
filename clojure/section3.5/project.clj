b6e3ddc02d76f8a849e7fb884e6a212229ebc74c
Jake McCrary <jakemcc@gmail.com>
2010-03-14 18:09:08 -0500

    started on section 3.5

---
+++ clojure/section3.5/project.clj
@@ -0,0 +1,5 @@
+(defproject SICP-section3-5 "1.0.0-SNAPSHOT"
+  :description "Solutions to section 3.5 of SICP"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
