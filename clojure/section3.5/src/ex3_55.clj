3fa07154e14dfd9f9ffef8c6cc4136b99ab1c8c5
Jake McCrary <jakemcc@gmail.com>
2010-03-14 20:21:46 -0500

    reworked exercise 3.55

--- clojure/section3.5/src/ex3_55.clj
+++ clojure/section3.5/src/ex3_55.clj
@@ -2,17 +2,19 @@
 
 (ns ex3-55)
 
-(defn partial-sum
-  [s]
-  ((fn ps [a b]
-     (cons a (lazy-seq (ps (+ a (first b))
-			   (rest b)))))
-   (first s)
-   (rest s)))
+(comment "an attempt"
+	 (defn partial-sum
+	   [s]
+	   ((fn ps [a b]
+	      (cons a (lazy-seq (ps (+ a (first b))
+				    (rest b)))))
+	    (first s)
+	    (rest s))))
 
-; If you want to just do it for the example in book this works.
-; Couldn't think of a cool way to do it in a similar fashion					;
-; (def ps (lazy-seq (cons (first integers) (map + ps (rest integers)))))
-
-
-  
+(defn partial-sum [s]
+  (lazy-seq
+   (cons
+    (first s)
+    (map +
+	 (rest s)
+	 (partial-sum s)))))
