37f778ab836525ec3bdb82fbf70e76bc33a6dcd8
Jake McCrary <jakemcc@gmail.com>
2010-03-29 08:06:20 -0500

    started work on ex3.70

---
+++ clojure/section3.5/src/ex3_70.clj
@@ -0,0 +1,17 @@
+; Exercise 3.70
+
+(ns ex3-70)
+
+(defn merge-weighted [weight s1 s2]
+  (cond (nil? s1) s2
+        (nil? s2) s1
+        :else
+        (let [x1 (first s1), x2 (first s2)]
+          (if (<= (weight x1) (weight x2))
+            (lazy-seq
+             (cons x1 (merge-weighted weight (next s1) s2)))
+            (lazy-seq
+             (cons x2 (merge-weighted weight s1 (next s2))))))))
+
+(merge-weighted identity '(1 1 1 1) '(2 2 2 2))
+(merge-weighted - '(1 1 1) '(2 2 2))
