0c2fb1581c1c2e03547ce54c36029d553f32ac0b
Jake McCrary <jakemcc@gmail.com>
2010-03-29 21:07:10 -0500

    finished exericse 3.70

--- clojure/section3.5/src/ex3_70.clj
+++ clojure/section3.5/src/ex3_70.clj
@@ -13,3 +13,40 @@
             (lazy-seq
              (cons x2 (merge-weighted weight s1 (next s2))))))))
 
+4(defn pairs [s t]
+  (lazy-seq
+   (cons
+    (list (first s) (first t))
+    (interleave
+     (map (fn [x] (list (first s) x))
+          (next t))
+     (pairs (next s) (next t))))))
+
+(defn weighted-pairs [weight s t]
+  (lazy-seq
+   (cons
+    (list (first s) (first t))
+    (merge-weighted weight
+                    (map (fn [x] (list (first s) x))
+                         (next t))
+                    (pairs (next s) (next t))))))
+
+; Look in ../test/test_ex3_70 for parts being tested
+; a)
+(take 5 (weighted-pairs #(+ (first %) (second %)) (iterate inc 1) (iterate inc 1)))
+
+; b)
+(def not-divisible
+     (filter #(not (or (zero? (rem % 2))
+                       (zero? (rem % 3))
+                       (zero? (rem % 5))))
+             (iterate inc 1)))
+
+(take 5 (weighted-pairs #(let [i (first %)
+                               j (second %)]
+                           (+ (* 2 i)
+                              (* 3 j)
+                              (* 5 i j)))
+                        not-divisible
+                        not-divisible))
+
--- clojure/section3.5/test/test_ex3_70.clj
+++ clojure/section3.5/test/test_ex3_70.clj
@@ -8,5 +8,9 @@
   (is (= '(1 1 2 2) (merge-weighted identity '(1 1) '(2 2))))
   (is (= '(2 2 1 1) (merge-weighted - '(1 1) '(2 2)))))
 
-
+(deftest show-part-a-complete
+  (is (= '((1 1) (1 2) (1 3) (2 2) (1 4))
+         (take 5 (weighted-pairs #(+ (first %) (second %))
+                                 (iterate inc 1)
+                                 (iterate inc 1))))))
 
