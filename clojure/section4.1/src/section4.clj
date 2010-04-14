1dc24c9cf24ac1d7e4bf81d30fbeb731f8a7a2af
Jake McCrary <jakemcc@gmail.com>
2010-04-13 23:32:36 -0500

    added analyze function

--- clojure/section4.1/src/section4.clj
+++ clojure/section4.1/src/section4.clj
@@ -214,3 +214,19 @@
     (if (< (count vars) (count vals))
       (Error. (str "Too many arguments supplied" vars vals))
       (Error. (str "Too few arguments supplied" vars vals)))))
+
+
+(defn analyze [exp]
+  (cond (self-evaluating? exp) 
+        (analyze-self-evaluating exp)
+        (quoted? exp) (analyze-quoted exp)
+        (variable? exp) (analyze-variable exp)
+        (assignment? exp) (analyze-assignment exp)
+        (definition? exp) (analyze-definition exp)
+        (if? exp) (analyze-if exp)
+        (lambda? exp) (analyze-lambda exp)
+        (begin? exp) (analyze-sequence (begin-actions exp))
+        (cond? exp) (analyze (cond->if exp))
+        (application? exp) (analyze-application exp)
+        :else
+        (Error. (str "Unknown expression type -- ANALYZE" exp))))
