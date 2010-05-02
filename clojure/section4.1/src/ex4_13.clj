4221ea817d1b7b4199f1bd1e09ce78941ece5a49
Jake McCrary <jakemcc@gmail.com>
2010-05-02 13:32:43 -0500

    added reasons for only removing from first frame in environment

--- clojure/section4.1/src/ex4_13.clj
+++ clojure/section4.1/src/ex4_13.clj
@@ -2,3 +2,9 @@
 
 ; Functionality has been added to environment.clj, environment_test.clj,
 ; section4.clj and section4_test.clj
+
+; I decided to only have the variable become unbound in the current
+; scope (whatever the first frame of the environment is). This seems to make
+; the most sense to me. Being able to traverse all the frames to remove
+; a binding seems like the wrong thing. Especially when you consider that
+; a variable could be bound at different frames.
