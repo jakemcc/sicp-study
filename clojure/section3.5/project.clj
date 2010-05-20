6971cffe4be5d65ee706acd36bc2368e42b6e821
Jake McCrary <jakemcc@gmail.com>
2010-05-19 21:52:59 -0500

    changed project.clj files to look to swank-clojure/1.2.1 for swanky stuff

--- clojure/section3.5/project.clj
+++ clojure/section3.5/project.clj
@@ -2,4 +2,4 @@
   :description "Solutions to section 3.5 of SICP"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
