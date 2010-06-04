f62ed81e9289f931f480286dbc08afafcb1a4172
Jake McCrary <jakemcc@gmail.com>
2010-06-03 21:54:33 -0500

    halfassed exercise 4.32

---
+++ clojure/section4.2/src/ex4_32.clj
@@ -0,0 +1,6 @@
+; Exercise 4.32
+
+; These lists are competely lazy. The car and the cdr
+; are delayed. This can be useful to buid infinate tree
+; structures and be able to manipulate them without
+; worrying the infiniteness.
